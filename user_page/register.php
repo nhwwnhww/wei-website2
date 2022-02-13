af24d361b5f5265738ea7081f86dfb897f0c094d
nhwwnhww <nhwenhww@gmail.com>
2022-02-13 20:55:12 +1000

    main

--- user_page/register.php
+++ user_page/register.php
@@ -1,21 +1,35 @@
-<?php
-$conn = new mysqli("localhost","nhwwnhww","123Wjnhwwnhww","website");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>error</title>
+</head>
+<body>
+    <?php
+    $conn = new mysqli("localhost","nhwwnhww","123Wjnhwwnhww","website");
 
-$username = $_POST['username'];
-$realname = $_POST['realname'];
-$password = $_POST['password'];
+    $username = $_POST['username'];
+    $realname = $_POST['realname'];
+    $password = $_POST['password'];
 
-$sql = "INSERT INTO `user`(`username`, `realname`, `password`)
- VALUES ('$username','$realname','$password')";
-$result = $conn->query($sql);
+    $sql = "INSERT INTO `user`(`username`, `realname`, `password`)
+    VALUES ('$username','$realname','$password')";
+    $result = $conn->query($sql);
 
-if (!$conn){
-    echo "error";
-}
+    if (!$conn){
+        echo "link database error";
+    }
 
-if (!$result){
-    echo "register error";
-}
-else {
-    header("Location:../test.html");
-}
+    if (!$result){
+        echo "register error";
+        echo "Please try again";
+    }
+    else {
+        header("Location:../test.html");
+    }
+    ?>
+<a href="./register.html">back</a>
+</body>
+</html>
